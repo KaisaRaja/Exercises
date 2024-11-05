2b5a18cab464fe3781fba6abd88c710b40c12606
Kaisa <raja.kaisa@gmail.com>
2024-11-05 15:23:58 +0200

    Päeva lõpp

---
+++ dayTwo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class dayTwo {
+
+    public static final double PI = 3.14;
+    public static final String APP_NAME = "MyApp";
+    public final int MAX_USERS = 100;
+    int number;
+    static String name = "John";
+
+    public static void main(String[] args) {
+        // Test code later
+        String manager = "John";
+        String manager2  = "Mary";
+        String manager3= "Jane";
+
+        System.out.println(manager);
+        System.out.println(manager + " " + manager2 + " " + manager3);
+        int lenghtOfManager = manager2.length();
+        System.out.println(lenghtOfManager);
+        //  System.out.println(MAX_USERS);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int n = scanner.nextInt();
+        System.out.println(++n);
+        System.out.println(n+13);
+
+
+
+    }
+
+
+}
